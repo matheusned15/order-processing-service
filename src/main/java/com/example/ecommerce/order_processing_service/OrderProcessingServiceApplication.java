51c1c227f21d46642045b66468257e0a82707182
matheusned15 <matheusned15@gmail.com>
2025-05-06 16:11:22 -0300

    chore: initial project setup with pom.xml and main application class

--- src/main/java/com/example/ecommerce/order_processing_service/OrderProcessingServiceApplication.java
+++ src/main/java/com/example/ecommerce/order_processing_service/OrderProcessingServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.ecommerce.order_processing_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.ecommerce")
 public class OrderProcessingServiceApplication {
 
 	public static void main(String[] args) {
