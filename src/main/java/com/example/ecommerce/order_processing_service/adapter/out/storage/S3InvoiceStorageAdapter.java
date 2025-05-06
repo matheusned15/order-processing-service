79c4aaba990b0b5108e02260cb096ecbc82d5f71
matheusned15 <matheusned15@gmail.com>
2025-05-06 15:36:17 -0300

    storage: add S3InvoiceStorageAdapter

---
+++ src/main/java/com/example/ecommerce/order_processing_service/adapter/out/storage/S3InvoiceStorageAdapter.java
@@ -0,0 +1,32 @@
+package com.example.ecommerce.order_processing_service.adapter.out.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Component
+@RequiredArgsConstructor
+public class S3InvoiceStorageAdapter {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    /**
+     * Faz upload de um PDF para o S3 e retorna a chave (path) gerada.
+     */
+    public String uploadInvoice(String orderId, byte[] pdfBytes) {
+        String key = "invoices/" + orderId + ".pdf";
+        PutObjectRequest req = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType("application/pdf")
+                .build();
+        s3Client.putObject(req, RequestBody.fromBytes(pdfBytes));
+        return key;
+    }
+}
