a9e3852521816f25dc9f8fc7b6a788216f0e3290
matheusned15 <matheusned15@gmail.com>
2025-05-06 15:35:06 -0300

    api: add OrderController and PaymentController

---
+++ src/main/java/com/example/ecommerce/order_processing_service/adapter/in/rest/OrderController.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce.order_processing_service.adapter.in.rest;
+
+import com.example.ecommerce.order_processing_service.application.dto.OrderRequestDTO;
+import com.example.ecommerce.order_processing_service.application.dto.OrderResponseDTO;
+// Aqui o import CORRETO para onde sua interface realmente está:
+import com.example.ecommerce.order_processing_service.application.service.IOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor           // gera um construtor com orderService como parâmetro
+public class OrderController {
+
+    private final IOrderService orderService;  // sem @Autowired
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<OrderResponseDTO> createOrder(@RequestBody OrderRequestDTO dto) {
+        return orderService.createOrder(dto);
+    }
+
+    @GetMapping("/{orderId}")
+    public Mono<OrderResponseDTO> getOrder(@PathVariable String orderId) {
+        return orderService.getOrderById(orderId);
+    }
+}
---
+++ src/main/java/com/example/ecommerce/order_processing_service/adapter/in/rest/PaymentController.java
@@ -0,0 +1,4 @@
+package com.example.ecommerce.order_processing_service.adapter.in.rest;
+
+public class PaymentController {
+}
