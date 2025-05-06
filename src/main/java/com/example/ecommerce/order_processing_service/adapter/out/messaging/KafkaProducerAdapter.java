dc0d60ff9007a8c9b34c6bb4ea1012efc271bc6e
matheusned15 <matheusned15@gmail.com>
2025-05-06 15:36:04 -0300

    messaging: add KafkaProducerAdapter

---
+++ src/main/java/com/example/ecommerce/order_processing_service/adapter/out/messaging/KafkaProducerAdapter.java
@@ -0,0 +1,23 @@
+package com.example.ecommerce.order_processing_service.adapter.out.messaging;
+
+
+import com.example.ecommerce.order_processing_service.common.event.OrderCreatedEvent;
+import com.example.ecommerce.order_processing_service.common.event.PaymentCompletedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaProducerAdapter {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishOrderCreated(OrderCreatedEvent event) {
+        kafkaTemplate.send("orders.created", event.getOrderId().toString(), event);
+    }
+
+    public void publishPaymentCompleted(PaymentCompletedEvent event) {
+        kafkaTemplate.send("orders.paid", event.getOrderId().toString(), event);
+    }
+}
